33f297eb98620b1b7dd5fe982d311eb5d52d3d30
Kotvaldo <adamstano5@gmail.com>
2024-02-20 18:59:17 +0100

    bottom_menu

--- app/src/main/java/com/example/bicycleeshop/MainActivity.kt
+++ app/src/main/java/com/example/bicycleeshop/MainActivity.kt
@@ -2,11 +2,42 @@ package com.example.bicycleeshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
+import com.example.bicycleeshop.Fragments.CartFragment
+import com.example.bicycleeshop.Fragments.CategoryFragment
+import com.example.bicycleeshop.Fragments.HomeFragment
+import com.example.bicycleeshop.databinding.ActivityMainBinding
+import com.google.android.material.bottomappbar.BottomAppBar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private var fragmentManager = supportFragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomnav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.cart -> replaceFragment(CartFragment())
+                R.id.category -> replaceFragment(CategoryFragment())
+                else -> {false  }
+            }
+
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment): Boolean {
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment)
+        fragmentTransaction.commit()
+        return true
     }
 
 }
