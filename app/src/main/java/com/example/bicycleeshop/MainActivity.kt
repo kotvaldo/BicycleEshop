015a25d8d0f91e12b5e13f00b3da9f9536e4b8a6
Kotvaldo <adamstano5@gmail.com>
2024-02-21 14:58:01 +0100

    drawer setup

--- app/src/main/java/com/example/bicycleeshop/MainActivity.kt
+++ app/src/main/java/com/example/bicycleeshop/MainActivity.kt
@@ -1,43 +1,53 @@
 package com.example.bicycleeshop
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.replace
-import com.example.bicycleeshop.Fragments.CartFragment
-import com.example.bicycleeshop.Fragments.CategoryFragment
-import com.example.bicycleeshop.Fragments.HomeFragment
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.bicycleeshop.databinding.ActivityMainBinding
-import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding : ActivityMainBinding
-    private var fragmentManager = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.content?.topAppBar)
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.fragmentContainerView)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_category, R.id.nav_cart
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+        bottomNav.setupWithNavController(navController)
 
-        binding.bottomnav.setOnItemSelectedListener {
-            when(it.itemId) {
-                R.id.home -> replaceFragment(HomeFragment())
-                R.id.cart -> replaceFragment(CartFragment())
-                R.id.category -> replaceFragment(CategoryFragment())
-                else -> {false  }
-            }
 
-        }
 
     }
 
-    private fun replaceFragment(fragment: Fragment): Boolean {
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragmentContainerView, fragment)
-        fragmentTransaction.commit()
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_drawer, menu)
         return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
 }
